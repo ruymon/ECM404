eaa1923c6d179f413db99caa1d905caaa6dec70f
Ruy Monteiro <ruystfrancis@gmail.com>
2025-03-05 22:59:18 -0300

    feat: finish ex6 - l2

--- L2/ex6.c
+++ L2/ex6.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-/* Enunciado - Faça um programa que leia um numero inteiro positivo par e,
+/* Enunciado - FaÃ§a um programa que leia um numero inteiro positivo par e,
  * a seguir, exiba todos os numeros pares de 0 ate o numero digitado em ordem
  * crescente, separados apenas por um espaco.
  *
@@ -20,7 +21,47 @@
  * 0 2 4 6 8 10 12 14
  */
 
-int main()
+bool isNumberOdd(int number)
 {
-    return 0;
+    return (number % 2) == 0;
+}
+
+bool isNumberPositive(int number)
+{
+    return number > 0;
+}
+
+bool isInputValueValid(int value)
+{
+    if (!isNumberPositive(value))
+    {
+        printf("Digite um numero positivo! \n");
+        return false;
+    }
+
+    if (!isNumberOdd(value))
+    {
+        printf("Digite um numero par! \n");
+        return false;
+    }
+
+    return true;
+}
+
+void main()
+{
+    int value, iterator;
+
+    printf("Numero: ");
+    scanf("%i", &value);
+
+    if (!isInputValueValid(value))
+        return;
+
+    for (iterator = 0; iterator <= value; iterator++)
+    {
+        isNumberOdd(iterator) && printf("%i ", iterator);
+    }
+
+    return;
 }
