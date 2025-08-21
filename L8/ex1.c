5facc67f97776f5706e5e3592965e58b071632b3
ruymon <ruystfrancis@gmail.com>
2025-08-21 19:06:16 -0300

    feat: L08 ex1

--- L8/ex1.c
+++ L8/ex1.c
@@ -1,16 +1,16 @@
 /*
  * Enunciado:
- *    Crie uma lista ligada de alunos, em que cada aluno deve consistir de um
- *    struct contendo o seu RA, 4 notas e a m�dia do aluno. O programa
- *    principal deve pedir para que o usu�rio digite o RA e as 4 notas de
- *    3 alunos, para a seguir exibir a lista ligada completa (use exibe_lista).
+ * Crie uma lista ligada de alunos, em que cada aluno deve consistir de um
+ * struct contendo o seu nome, 4 notas e a média do aluno. O programa
+ * principal deve pedir para que o usuário digite o nome e as 4 notas de
+ * 3 alunos, para a seguir exibir a lista ligada completa (use exibe_lista).
  *
  * Exemplos:
  *
- *    Aluno: Rodrigo 2 3 4 5
- *    Aluno: Alexandre 10 10 8 9
- *    Aluno: Diego 7 8 10 10
- *    (Rodrigo | 3.50) -> (Alexandre | 9.25) -> (Diego | 8.75)
+ * Aluno: Rodrigo 2 3 4 5
+ * Aluno: Alexandre 10 10 8 9
+ * Aluno: Diego 7 8 10 10
+ * (Rodrigo | 3.50) -> (Alexandre | 9.25) -> (Diego | 8.75)
  *
  */
 
@@ -19,11 +19,13 @@
 #include <string.h>
 
 #define MAX 100
+#define NUM_NOTAS 4
+#define NUM_ALUNOS 3
 
 typedef struct
 {
   char nome[MAX];
-  float notas[4];
+  float notas[NUM_NOTAS];
   float media;
 } Aluno;
 
@@ -33,8 +35,6 @@ typedef struct SLista
   struct SLista *prox;
 } TLista;
 
-/* Fun��es recomendadas (n�o precisa implementar nenhuma delas se n�o quiser)
- */
 Aluno ler_aluno();
 void exibe_aluno(Aluno a);
 float calcula_media(float *notas);
@@ -43,34 +43,102 @@ TLista *cria_lista();
 TLista *destroi_lista(TLista *alunos);
 void insere_no_fim(TLista *alunos, Aluno aluno);
 
-/* Fun��es para uso livre */
-void exibe_lista(TLista *alunos); // exibe uma lista de alunos
-void ler_string(char *s);         // le uma string de forma "segura"
+void exibe_lista(TLista *alunos);
+void ler_string(char *s);
 
 int main(int argc, char *argv[])
 {
+  TLista *lista_de_alunos = cria_lista();
+
+  for (int i = 0; i < NUM_ALUNOS; i++)
+  {
+    printf("--- Aluno %d ---\n", i + 1);
+    Aluno novo_aluno = ler_aluno();
+    insere_no_fim(lista_de_alunos, novo_aluno);
+  }
+
+  printf("\n--- Lista de Alunos ---\n");
+  exibe_lista(lista_de_alunos);
+
+  // Libera a memória alocada para a lista
+  lista_de_alunos = destroi_lista(lista_de_alunos);
 
   return EXIT_SUCCESS;
 }
 
 Aluno ler_aluno()
 {
+  Aluno a;
+
+  printf("Digite o nome do aluno: ");
+  ler_string(a.nome);
+
+  printf("Digite as %d notas do aluno: ", NUM_NOTAS);
+  for (int i = 0; i < NUM_NOTAS; i++)
+  {
+    scanf("%f", &a.notas[i]);
+  }
+
+  a.media = calcula_media(a.notas);
+  return a;
 }
 
 float calcula_media(float *notas)
 {
+  float soma = 0.0f;
+  for (int i = 0; i < NUM_NOTAS; i++)
+  {
+    soma += notas[i];
+  }
+  return soma / NUM_NOTAS;
 }
 
 TLista *cria_lista()
 {
+  TLista *cabeca = (TLista *)malloc(sizeof(TLista));
+  if (cabeca == NULL)
+  {
+    perror("Erro de alocacao de memoria");
+    exit(EXIT_FAILURE);
+  }
+  cabeca->prox = NULL;
+  return cabeca;
 }
 
 TLista *destroi_lista(TLista *alunos)
 {
+  TLista *atual = alunos;
+  TLista *proximo_no;
+
+  while (atual != NULL)
+  {
+    proximo_no = atual->prox; // Guarda a referência para o próximo
+    free(atual);              // Libera o nó atual
+    atual = proximo_no;       // Move para o próximo
+  }
+
+  return NULL;
 }
 
 void insere_no_fim(TLista *alunos, Aluno aluno)
 {
+  TLista *novo_no = (TLista *)malloc(sizeof(TLista));
+  if (novo_no == NULL)
+  {
+    perror("Erro de alocacao de memoria");
+    exit(EXIT_FAILURE);
+  }
+  novo_no->aluno = aluno;
+  novo_no->prox = NULL;
+
+  // Percorre a lista até encontrar o último nó
+  TLista *aux = alunos;
+  while (aux->prox != NULL)
+  {
+    aux = aux->prox;
+  }
+
+  aux->prox = novo_no;
 }
 
 void exibe_aluno(Aluno a)
@@ -80,7 +148,7 @@ void exibe_aluno(Aluno a)
 
 void exibe_lista(TLista *alunos)
 {
-  TLista *aux = alunos->prox;
+  TLista *aux = alunos->prox; // Pula o nó cabeça
   while (aux != NULL)
   {
     exibe_aluno(aux->aluno);
@@ -93,7 +161,7 @@ void exibe_lista(TLista *alunos)
 
 void ler_string(char *s)
 {
-  scanf("\n");
+  scanf("\n"); // Limpa o buffer do teclado (consome o '\n' de leituras anteriores)
   fgets(s, MAX, stdin);
   size_t tam = strlen(s);
   if (s[tam - 1] == '\n')
